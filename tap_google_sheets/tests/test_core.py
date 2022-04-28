e7e853d4c0d9c371f950ade174aac67aa44dba82
danielpdwalker <danielpdwalker@gmail.com>
2022-04-28 10:14:19 +0100

    Updated and added to core tests

--- tap_google_sheets/tests/test_core.py
+++ tap_google_sheets/tests/test_core.py
@@ -1,23 +1,48 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-import datetime
+import unittest
 
+import responses
 from singer_sdk.testing import get_standard_tap_tests
 
+import tap_google_sheets.tests.utils as test_utils
 from tap_google_sheets.tap import TapGoogleSheets
 
-SAMPLE_CONFIG = {
-    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d")
-    # TODO: Initialize minimal tap config
-}
 
+class TestCore(unittest.TestCase):
+    """Test class for core tap tests."""
 
-# Run standard built-in tap tests from the SDK:
-def test_standard_tap_tests():
-    """Run standard tap tests from the SDK."""
-    tests = get_standard_tap_tests(TapGoogleSheets, config=SAMPLE_CONFIG)
-    for test in tests:
-        test()
+    def setUp(self):
+        # reset mock responses
+        responses.reset()
 
+        self.mock_config = test_utils.MOCK_CONFIG
 
-# TODO: Create additional tests as appropriate for your tap.
+    def test_base_credentials_discovery(self):
+        """Test basic discover sync"""
+
+        catalog = TapGoogleSheets(self.mock_config).discover_streams()
+
+        # expect valid catalog to be discovered
+        self.assertEqual(len(catalog), 1, "Total streams from default catalog")
+
+    # Run standard built-in tap tests from the SDK:
+    @responses.activate()
+    def test_standard_tap_tests(self):
+        """Run standard tap tests from the SDK."""
+        responses.add(
+            responses.POST,
+            "https://oauth2.googleapis.com/token",
+            json={"access_token": "new_token"},
+            status=200,
+        ),
+        responses.add(
+            responses.GET,
+            "https://sheets.googleapis.com/v4/spreadsheets/12345/values/Sheet1",
+            json={"values": [["1"], ["2"]]},
+            status=200,
+        )
+
+        tests = get_standard_tap_tests(TapGoogleSheets, config=self.mock_config)
+        for test in tests:
+            test()
---
+++ tap_google_sheets/tests/utils.py
@@ -0,0 +1,39 @@
+"""Test utilities."""
+
+from singer_sdk.helpers import _catalog
+from singer_sdk.helpers._singer import Catalog
+
+from tap_google_sheets.tap import TapGoogleSheets
+
+SINGER_MESSAGES = []
+
+MOCK_CONFIG = {
+    "client_id": "123",
+    "client_secret": "123",
+    "refresh_token": "123",
+    "sheet_id": "12345",
+}
+
+
+def accumulate_singer_messages(message):
+    """Collect singer library write_message in tests."""
+    SINGER_MESSAGES.append(message)
+
+
+def set_up_tap_with_custom_catalog(mock_config, stream_list):
+    """Create an instance of tap-spotify with specific config and streams."""
+    tap = TapGoogleSheets(config=mock_config)
+    # Run discovery
+    tap.run_discovery()
+    # Get catalog from tap
+    catalog = Catalog.from_dict(tap.catalog_dict)
+    # Reset and re-initialize with an input catalog
+    _catalog.deselect_all_streams(catalog=catalog)
+    for stream in stream_list:
+        _catalog.set_catalog_stream_selected(
+            catalog=catalog,
+            stream_name=stream,
+            selected=True,
+        )
+    # Initialise tap with new catalog
+    return TapGoogleSheets(config=mock_config, catalog=catalog.to_dict())
