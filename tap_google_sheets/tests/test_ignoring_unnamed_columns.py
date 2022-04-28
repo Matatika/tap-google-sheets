fc64dcb39ab9251cde2cc2753905971b286aa93a
danielpdwalker <danielpdwalker@gmail.com>
2022-04-28 17:27:49 +0100

    Added test for ignoring unnamed columns

---
+++ tap_google_sheets/tests/test_ignoring_unnamed_columns.py
@@ -0,0 +1,56 @@
+"""Tests standard tap features using the built-in SDK tests library."""
+
+import unittest
+
+import responses
+import singer
+
+import tap_google_sheets.tests.utils as test_utils
+from tap_google_sheets.tap import TapGoogleSheets
+
+
+class TestCore(unittest.TestCase):
+    """Test class for core tap tests."""
+
+    def setUp(self):
+        self.mock_config = test_utils.MOCK_CONFIG
+
+        responses.reset()
+        del test_utils.SINGER_MESSAGES[:]
+
+        singer.write_message = test_utils.accumulate_singer_messages
+
+    @responses.activate()
+    def test_ignoring_unnamed_columns(self):
+
+        self.missing_column_response = {
+            "values": [["Column_One", "", "Column_Two"], [1, 1, 1]]
+        }
+
+        tap = TapGoogleSheets(config=self.mock_config)
+
+        """Run standard tap tests from the SDK."""
+        responses.add(
+            responses.POST,
+            "https://oauth2.googleapis.com/token",
+            json={"access_token": "new_token"},
+            status=200,
+        ),
+        responses.add(
+            responses.GET,
+            "https://sheets.googleapis.com/v4/spreadsheets/12345/values/Sheet1",
+            json=self.missing_column_response,
+            status=200,
+        )
+
+        tap.sync_all()
+
+        self.assertEqual(len(test_utils.SINGER_MESSAGES), 4)
+        self.assertIsInstance(test_utils.SINGER_MESSAGES[0], singer.SchemaMessage)
+        self.assertIsInstance(test_utils.SINGER_MESSAGES[1], singer.SchemaMessage)
+        self.assertIsInstance(test_utils.SINGER_MESSAGES[2], singer.RecordMessage)
+        self.assertIsInstance(test_utils.SINGER_MESSAGES[3], singer.StateMessage)
+
+        self.assertEquals(
+            test_utils.SINGER_MESSAGES[2].record, {"Column_One": 1, "Column_Two": 1}
+        )
